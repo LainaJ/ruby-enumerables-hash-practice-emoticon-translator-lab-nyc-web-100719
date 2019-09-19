d4ee0e066193c02d33264a0b143458eada697bd9
Laina Karosic <lainakarosic@gmail.com>
2019-09-19 17:39:36 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,7 +17,7 @@ end
 
 def get_japanese_emoticon(file_path, emoticons)
   load_library(file_path)
-  [:get_emoticon]["grinning"] 
+  emoticons[:get_emoticon]["grinning"] 
 end
 
 def get_english_meaning
