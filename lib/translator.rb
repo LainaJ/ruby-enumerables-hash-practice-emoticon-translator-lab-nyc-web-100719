e3386e2af063bdc719041944e786c3a79ce68bff
Laina Karosic <lainakarosic@gmail.com>
2019-09-19 18:11:26 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,7 +8,7 @@ def load_library(file_path)
 
   emoticons.each do |key, value|
     new_hash[:get_meaning][value[1]] = key
-    #the number [1] talks about emoticon ARRAY positions, then places it as equal to key position with hash that we're creating
+    #I think the number [1] talks about emoticon ARRAY positions, then places it as equal to key position with hash that we're creating
     new_hash[:get_emoticon][value[0]] = emoticons[key][1]
 
   end
@@ -24,7 +24,7 @@ def get_japanese_emoticon(file_path, emoticon)
   end
 end
 
-def get_english_meaning(file_path, emoticon)
+def get_english_meaning(file_path, value)
   load_library(file_path)
-  emoticon_on_demand = load_library(file_path)[:get_emoticon][meaning]
+  emoticon_on_demand = load_library(file_path)[:get_meaning][value]
 end
