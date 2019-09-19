72665fd9a6abe2d5db80c1d082ebe7399ee852f3
Laina Karosic <lainakarosic@gmail.com>
2019-09-19 17:54:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,8 +18,7 @@ end
 def get_japanese_emoticon(file_path, emoticon)
   load_library(file_path)
     emoticon_on_demand = load_library(file_path)[:get_emoticon][emoticon] if emoticon_on_demand
-  else false
-    p "Sorry, that emoticon was not found"
+  else false p "Sorry, that emoticon was not found"
     end
   end 
   return emoticon_on_demand
